40b5df329cb4855aafff58f6f2463418041fd5cd
PanagiotaTsitsika <59533430+PanagiotaTsitsika@users.noreply.github.com>
2020-03-12 21:31:36 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static string Likes(string[]names)
+        {
+            if(names.Length == 0)
+            {
+                return "No one likes this";
+            }
+            else if(names.Length == 1)
+            {
+                return $"{names[0]} like this";
+            }
+            else if(names.Length == 2)
+            {
+                return $"{names[0]} and {names[1]} like this";
+            }
+            else if(names.Length == 3)
+            {
+                return $"{names[0]}, {names[1]} and {names[2]} like this";
+            }
+            else
+            {
+                return $"{names[0]}, {names[1]} and {names.Length - 2} like this";
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string[] names = { "Nick", "Alex", "Maria", "Marios"};
+            Console.WriteLine(Likes(names));
+        }
+    }
+}
